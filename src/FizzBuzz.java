b9b674414109feb6ff4e199595d0757707d6bd5b
Melanie Johnson <meljohn23@msn.com>
2016-08-18 13:35:07 -0400

    my previous commit had formatting mixed up, so i wanted to redo it so mine matches what assignment asks for

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,16 +4,16 @@ public class FizzBuzz {
         for (int x = 1; x <= 100; x = x + 1) {
 
             if ((x % 3) == 0 && (x % 5) == 0) {
-                System.out.println(" FizzBuzz ");
+                System.out.println("FizzBuzz");
 
             } else if ((x % 5) == 0) {
-                System.out.print(" Buzz ");
+                System.out.println("Buzz");
 
             } else if ((x % 3) == 0) {
-                System.out.print(" Fizz ");
+                System.out.println("Fizz");
 
             } else {
-                System.out.print(x + " ");
+                System.out.println(x);
             }
         }
     }
